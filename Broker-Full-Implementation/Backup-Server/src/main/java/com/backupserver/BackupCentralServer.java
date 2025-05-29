4d5ab91cb44598f4fb8e357a5b52f2723cff46a9
david-rodj <rodriguezj.david@javeriana.edu.co>
2025-05-29 00:29:07 -0500

    Update BackupCentralServer.java

--- Broker-Full-Implementation/Backup-Server/src/main/java/com/backupserver/BackupCentralServer.java
+++ Broker-Full-Implementation/Backup-Server/src/main/java/com/backupserver/BackupCentralServer.java
@@ -74,7 +74,7 @@ public class BackupCentralServer {
 
         new Thread(() -> {
 
-            try (ZContext context = new ZContext()) {
+            try (ZContext healthContext = new ZContext()) {
 
                 ZMQ.Socket responder = context.createSocket(SocketType.REP);
 
